a2cca21706c78bfbf1c047cf15fa3a029d5fd707
tizianodemarco <127427719+tizianodemarco@users.noreply.github.com>
2023-08-12 16:59:30 -0300

    modulo point

--- Point.hs
+++ Point.hs
@@ -4,4 +4,7 @@ module Point ( Point, newP, difP)
 data Point = Poi Int Int deriving (Eq, Show)
 
 newP :: Int -> Int -> Point
+newP a b = Poi a b
+
 difP :: Point -> Point -> Float  -- distancia absoluta
+difP (Poi x1 y1) (Poi x2 y2) = round(sqrt((fromIntegral(x1-x2))^2+(fromIntegral(y1-y2))^2))
