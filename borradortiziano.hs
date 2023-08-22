c121386dd51d4dccadc53e2176be331b8273a989
anatissera <126919011+anatissera@users.noreply.github.com>
2023-08-22 20:50:57 -0300

    borrador tiziano

---
+++ borradortiziano.hs
@@ -0,0 +1,18 @@
+import City 
+import Quality 
+import Tunel 
+import Link
+import Point
+import Data.Typeable
+data Region = Reg [City] [Link] [Tunel] deriving Show
+
+foundR :: Region -> City -> Region
+foundR (Reg cities links tunnels) newCity
+    | not (isCityType newCity) = error "InvalidCityType: The second argument must be of type City"
+    | any (\existingCity -> existingCity == newCity) cities = error "CityAlreadyExists: A city with the same name already exists in the region."
+    | otherwise = Reg (newCity : cities) links tunnels
+  where
+    isCityType :: City -> Bool
+    isCityType city = case cast city of
+        Just (_ :: City) -> True
+        Nothing -> False
