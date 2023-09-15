99d0b80c2f99a64e08d113d03dea2f177a5f2bb6
anatissera <atissera@udesa.edu.ar>
2023-09-15 16:13:50 -0300

    cambios de vuelta?

--- TP2/queue/src/queue/QueueTest.java
+++ TP2/queue/src/queue/QueueTest.java
@@ -1,10 +1,9 @@
 package queueTester;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QueueTest {
 	
