cb5dcc1415a27018398a75e6594a4ddc4b73c004
anatissera <atissera@udesa.edu.ar>
2023-10-08 20:20:51 -0300

    mod 1

--- TP3/src/Nemo/Coordinates.java
+++ TP3/src/Nemo/Coordinates.java
@@ -15,44 +15,88 @@ public class Coordinates {
 	public String getDirection() { return direction; }
 	
 	public Coordinates modify(String whereTo) {
-	
-		// puede ser 
-		 if (whereTo == "d") {
-			 this.z -= 1;
-		 }
-		 if (whereTo == "u") {
-			 this.z += 1;
-		 }
-		 if (whereTo == "f") {
-			 // si mira al norte mueve y +1
-			 // si mira al sur mueuve  y -1
-			 // si mira al este mueve x +1 
-			 // si mira al oeste mueve x -1
-			 if (this.direction == "North") {
-				 this.y += 1;
-			 }
-			 if (this.direction == "South") {
-				 this.y -= 1;
-			 }
-			 if (this.direction == "East") {
-				 this.x += 1;
-			 }
-			 if (this.direction == "West") {
-				 this.x -= 1;
-			 }
+
+//		- 'd' que le indica a Nemo descender una unidad.
+//		- 'u' que le indica a Nemo ascender una unidad.
+//		- 'l' que le indica a Nemo rotar 90 grados a izquierda.
+//		- 'r' que le indica a Nemo rotar 90 grados a derecha.
+//		- 'f' que le indica a Nemo avanzar una unidad.
+//		- 'm' que le indica a Nemo liberar la cápsula.
+		// la función puede recibir un string con una secuencia de comandos, por ejemplo "ddrffl" y debe ejecutarlos en orden.
+		// "ddm" tira error porque no se puede lanzar la cápsula a menos de -1 metros de profundidad,
+		// o sea solo se puede lanzar en la superficie (0) o el primer nivel de profundidad (-1).
+		// "uuuu" no hace nada porque no se puede ascender más allá de la superficie.
+
+		for (int= 0, i < whereTo.length(), i++){
+			if (whereTo.equals("d")) {
+				z -= 1;
+			}
+			if (whereTo.equals("u")) {
+				z += 1;
+			}
+			if (whereTo.equals("l")) {
+				if (direction.equals("North")) {
+					direction = "West";
+				}
+				else if (direction.equals("West")) {
+					direction = "South";
+				}
+				else if (direction.equals("South")) {
+					direction = "East";
+				}
+				else if (direction.equals("East")) {
+					direction = "North";
+				}
+			}
+			if (whereTo.equals("r")) {
+				if (direction.equals("North")) {
+					direction = "East";
+				}
+				else if (direction.equals("East")) {
+					direction = "South";
+				}
+				else if (direction.equals("South")) {
+					direction = "West";
+				}
+				else if (direction.equals("West")) {
+					direction = "North";
+				}
+			}
+			if (whereTo.equals("f")) {
+				if (direction.equals("North")) {
+					y += 1;
+				}
+				else if (direction.equals("East")) {
+					x += 1;
+				}
+				else if (direction.equals("South")) {
+					y -= 1;
+				}
+				else if (direction.equals("West")) {
+					x -= 1;
+				}
+				if (whereTo.equals("m")) {
+					if (z < -1) {
+						throw new RunTimeException("No se puede lanzar la cápsula por debajo del primer nivel de inmersión");
+					}
+					else {
+						// lanzar la cápsula
+						// no tiene ningún efecto detectable en el submarino.
+						// ¿No hay que hacer nada?
+					}
+				}
 		}
-	 
-		 if (whereTo == "l") {
-			 this.direction = "West"; 
-		 }
-		 if (whereTo == "r") {
-			 this.direction = "Este";
-		 }
-	
-	
-	return null;
-	 
- }
+
+		}
+		return null;
+
+//		no hay problemas con sumergirse demasiado, tampoco pasa nada si le insistimos
+//		en emerger cuando está en superficie.
+//		la cápsula sólo se puede lanzar en superficie o en el primer nivel de inmersión.
+//		En estos casos lanzar la cápsula no tiene ningún efecto detectable en el submarino.
+//		Pero si se intenta liberar a mayor profundidad de lo permitido el submarino se destruye.
+
+	}
 	
 	
 
--- TP3/src/Nemo/NemoTest.java
+++ TP3/src/Nemo/NemoTest.java
@@ -19,13 +19,12 @@ public class NemoTest {
 	  assertEquals(sub.getDepth(), 0);
 	  assertEquals(sub.position_X(), 0);
 	  assertEquals(sub.position_Y(), 0);
-	  
-//	  assertEquals(sub.getOrientation(), "Norte");
+        assertEquals(sub.getOrientation(), "North");
 	  
   } // lo que pasa con la profundidad no es lo mismo que en la superficie
   // la profundidad explota
   // la superficie no
-  
+
   //coordenada inicial, posición en superficie. ¿Cuál es la posición?
   
   //dirección: a dónde mira ¿Dónde está mirando?
@@ -36,6 +35,8 @@ public class NemoTest {
 	  sub.move("");
 	  assertEquals(0, sub.position_X());
 	  assertEquals(0, sub.position_Y());
+        assertEquals(0, sub.getDepth());
+        assertEquals("North", sub.getOrientation());
 	  
   }
   @Test
@@ -43,8 +44,8 @@ public class NemoTest {
 	  Submarine sub = new Submarine();
 	  int initialDepth = sub.getDepth();
 	  sub.move("d");
-	  
-	  assertEquals (initialDepth -= 1, sub.getDepth());
+
+      assertEquals (initialDepth -= 1, sub.getDepth());
 	 
 	  
   }
@@ -57,46 +58,70 @@ public class NemoTest {
 	  assertEquals (initialDepth += 1, sub.getDepth());
   }
   @Test
-  public void Test04() {
+  public void Test04() { // se mueve hacia adelante
+        Submarine sub = new Submarine();
+        int initialDepth = sub.getDepth();
+        sub.move("f");
+
+        assertEquals (initialDepth, sub.getDepth());
 	  
   }
   @Test
-  public void Test05() {
+  public void Test05() { // rotar a la izquierda
+        Submarine sub = new Submarine();
+        int initialDepth = sub.getDepth();
+        sub.move("l");
+
+        assertEquals (initialDepth, sub.getDepth());
 	  
   }
   @Test
-  public void Test06() {
-	  
-  }
+  public void Test06() { // rotar a la derecha
+        Submarine sub = new Submarine();
+        int initialDepth = sub.getDepth();
+        sub.move("r");
+
+        assertEquals (initialDepth, sub.getDepth());
+
   @Test
   public void Test07() {
+          // no pasa nada si le insistimos en emerger cuando está en superficie.
+          Submarine sub = new Submarine();
+          sub.move("uuuu");
+          // verificar que no emerge más de lo que puede (arriba de 0)
+          assertEquals(0, sub.getDepth());
 	  
   }
   @Test
   public void Test08() {
-	  
+          //no hay problemas con sumergirse demasiado
+          Submarine sub = new Submarine();
+          sub.move("dddd");
+          // verificar que no tira error
+
   }
   @Test
   public void Test09() {
+      // la cápsula sólo se puede lanzar en superficie o en el primer nivel de inmersión.
+      //	En estos casos lanzar la cápsula no tiene ningún efecto detectable en el submarino.
+      // (verificar que "dd" no tira error)
+
 	  
   }
   @Test
   public void Test10() {
-	  
+      //	Si se intenta liberar a mayor profundidad de lo permitido el submarino se destruye.
+      // (verificar que "ddm" tira error)
+
   }
   @Test
   public void Test11() {
 	  
   }
-  @Test
-  public void Test12() {
-	  
-  }
   
-//  private void assertThrowsLike( String message, Executable executable ) {
-//	    
-//	    assertEquals( message,
-//	                  assertThrows( Exception.class, executable ).getMessage() );
-//	  }
+  private void assertThrowsLike( String message, Executable executable ) {
+	    assertEquals( message,
+                assertThrows( Exception.class, executable ).getMessage() );
+	  }
   
 }
