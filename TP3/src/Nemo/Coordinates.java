d56b5d90025b1a3620880f9ec3a0db97cedc2351
anatissera <atissera@udesa.edu.ar>
2023-10-15 17:17:46 -0300

    Static ints?

--- TP3/src/Nemo/Coordinates.java
+++ TP3/src/Nemo/Coordinates.java
@@ -2,40 +2,27 @@ package Nemo;
 
 public class Coordinates {
 	
-	public int x;
-	public int y;
+	public static int x;
+	public static int y;
 
     public Coordinates(int x, int y) {
-        this.x = x;
-        this.y = y;
+        Coordinates.x = x;
+        Coordinates.y = y;
     }
 	
 	public int getX() { return x; }
 	public int getY() { return y; }
 
-//	  public void moveEast() {
-//	        x += 1;
-//	    }
-//	  public void moveWest() {
-//	        x -= 1;
-//	    }
-//	  public void moveNorth() {
-//	        y += 1;
-//	    }
-//	  public void moveSouth() {
-//	        y -= 1;
-//	    }
-
-	  public Coordinates moveEast() {
-		  return new Coordinates(x + 1, y);
-	    }
-	  public Coordinates moveWest() {
-		  return new Coordinates(x - 1, y);
-	    }
-	  public Coordinates moveNorth() {
-		  return new Coordinates(x, y + 1);
-	    }
-	  public Coordinates moveSouth() {
-		  return new Coordinates(x, y - 1);
-	    }
+	public Coordinates moveEast() {
+		return new Coordinates(x + 1, y);
+	}
+	public Coordinates moveWest() {
+		return new Coordinates(x - 1, y);
+	}
+	public Coordinates moveNorth() {
+		return new Coordinates(x, y + 1);
+	}
+	public Coordinates moveSouth() {
+		return new Coordinates(x, y - 1);
+	}
 }
