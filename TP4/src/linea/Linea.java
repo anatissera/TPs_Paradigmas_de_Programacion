229f1111d54b9583aa6b3625cdcaf72e770c8077
anatissera <atissera@udesa.edu.ar>
2023-11-01 19:48:22 -0300

    comentarios

--- TP4/src/linea/Linea.java
+++ TP4/src/linea/Linea.java
@@ -32,7 +32,7 @@ public class Linea {
 	    private Turno turno;
 	    private Triunfo triunfo;
 	    
-	    public Linea(int base, int altura, char varianteTriunfo) {
+	    public Linea(int base, int altura, char estrategia) {
 	      
 	        this.base = base;
 	        this.altura = altura;
@@ -43,8 +43,9 @@ public class Linea {
 	        this.columnas = IntStream.range(0, base)
 	                .mapToObj(i -> new ArrayList<Character>())
 	                .collect(Collectors.toList());
-	        turno = new Turno('R');
-	        triunfo = InitializeTriunfo.createTriunfo(varianteTriunfo);
+	        
+	        turno = new Turno('X'); // X es rojas
+	        triunfo = InitializeTriunfo.createTriunfo(estrategia);
 	    } 
 	   // Referencia a reporte balance
 	 
@@ -53,7 +54,7 @@ public class Linea {
 	    }
 
 	    public boolean playBlueAt(int columna) {
-	        return play('O', columna);
+	        return play('O', columna); // O es azules
 	    }
 
 	    private boolean play(char color, int columna) {
@@ -108,3 +109,30 @@ public class Linea {
 	    
     // no hay clase fichas, jugador, ni tablero
 }
+
+
+// La clase principal es Linea, que representa el juego "4 en línea". Esta clase tiene un atributo columnas, 
+// que es una lista de listas que simula el tablero. Cada lista interna representa una columna en el juego 
+// y contiene caracteres que representan las fichas de los jugadores.
+//
+// Para gestionar las filas, se utiliza el método alturaMaxActual(). Este método recorre todas las columnas 
+// y devuelve la altura máxima actual del juego, es decir, la cantidad de fichas en la columna más alta. 
+// Esto se hace utilizando Stream para mapear el tamaño de cada columna y luego calcular el máximo con reduce.
+//
+// La representación de las filas se gestiona en el método getRow(fila). Este método crea una fila del juego, 
+// que es una cadena de caracteres, combinando las fichas en la misma fila de todas las columnas. Se utiliza 
+// IntStream.range(0, base) para recorrer las columnas y preguntarAt(columna, fila) para obtener el carácter en la posición actual.
+//
+// El método preguntarAt(columna, fila) verifica si hay una ficha en la posición especificada en el tablero. 
+// Si la columna contiene una ficha en la fila proporcionada, se devuelve el carácter correspondiente 
+// (que puede ser 'X' para las fichas rojas, 'O' para las fichas azules o ' ' para espacios vacíos). 
+// Si no hay una ficha en esa posición, se devuelve un espacio en blanco.
+//
+// El método ColumnIsFull(columna) verifica si una columna está llena, es decir, si ya contiene la cantidad 
+// máxima de fichas permitidas en esa columna. Esto se hace comprobando si el tamaño de la lista interna de 
+// la columna es igual o mayor que la altura máxima permitida
+
+// En resumen, la gestión de las filas en este código se realiza mediante la creación y manipulación de una 
+// estructura de datos de lista de listas, donde cada lista interna representa una columna y contiene las 
+// fichas de los jugadores en el orden en el que se han colocado. La altura máxima actual se calcula 
+// recorriendo todas las columnas, y las filas se generan concatenando las fichas correspondientes de cada columna.
